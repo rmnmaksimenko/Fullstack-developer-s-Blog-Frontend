61a389154f9de5beb3c72b4ae4dd49d2c19bb42d
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2023-03-17 13:51:06 +0200

    init

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="fullstack-developer-s-blog-frontend">
+    <BrowserRouter basename="blog">
       <App />
     </BrowserRouter>
   </React.StrictMode>
